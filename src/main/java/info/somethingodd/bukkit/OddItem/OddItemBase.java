a444b54ec9d16723a75aeb41a3be664923e67496
petteyg <petteyg359@gmail.com>
2012-01-23 16:10:06 -0600

    rabid mulberries

--- src/main/java/info/somethingodd/bukkit/OddItem/OddItemBase.java
+++ src/main/java/info/somethingodd/bukkit/OddItem/OddItemBase.java
@@ -16,15 +16,6 @@ package info.somethingodd.bukkit.OddItem;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.logging.Logger;
 
 /**
@@ -33,7 +24,6 @@ import java.util.logging.Logger;
 public class OddItemBase extends JavaPlugin {
     protected Logger log = null;
     protected String logPrefix = null;
-    protected String configFile = null;
 
     @Override
     public void onDisable() {
@@ -41,7 +31,6 @@ public class OddItemBase extends JavaPlugin {
         OddItem.clear();
         logPrefix = null;
         log = null;
-        configFile = null;
     }
 
     @Override
@@ -49,10 +38,9 @@ public class OddItemBase extends JavaPlugin {
         log = Bukkit.getServer().getLogger();
         logPrefix = "[" + getDescription().getName() + "] ";
         log.info(logPrefix + getDescription().getVersion() + " enabled");
-        configFile = getDataFolder() + System.getProperty("file.separator") + "OddItem.yml";
         try {
             OddItemConfiguration configuration = new OddItemConfiguration(this);
-            configuration.configure(configFile);
+            configuration.configure();
         } catch (Exception e) {
             log.severe(logPrefix + "Configuration error!");
             e.printStackTrace();
@@ -61,7 +49,7 @@ public class OddItemBase extends JavaPlugin {
         log.info(logPrefix + OddItem.itemMap.size() + " aliases loaded.");
     }
 
-    private void download() {
+    /*private void download() {
         BufferedReader bufferedReader;
         BufferedWriter bufferedWriter;
         String line;
@@ -97,5 +85,5 @@ public class OddItemBase extends JavaPlugin {
         } catch (MalformedURLException e) {
             log.severe(logPrefix + "Failed to construct update URL...");
         }
-    }
+    }*/
 }
