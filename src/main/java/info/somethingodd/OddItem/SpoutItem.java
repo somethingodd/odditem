737d5e0938e8f8ef5b59aebb2c9c3f500bef3743
Gordon Pettey <petteyg359@gmail.com>
2012-04-20 13:06:01 -0500

    start some spout stuff

---
+++ src/main/java/info/somethingodd/OddItem/SpoutItem.java
@@ -0,0 +1,55 @@
+/* This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package info.somethingodd.OddItem;
+
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.Plugin;
+import org.getspout.spoutapi.block.design.Texture;
+import org.getspout.spoutapi.material.block.GenericCubeCustomBlock;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Gordon Pettey (petteyg359@gmail.com)
+ */
+public class SpoutItem extends GenericCubeCustomBlock {
+    private static final Map<ItemStack, GenericCubeCustomBlock> customBlocks;
+    private ItemStack itemStack;
+    private String name;
+    private OddItemBase oddItemBase;
+    private Texture texture;
+
+    static {
+        customBlocks = new HashMap<ItemStack, GenericCubeCustomBlock>();
+    }
+
+    public SpoutItem(Plugin plugin, String name, String texture, int textureSize) {
+        super(plugin, name, texture, textureSize);    //To change body of overridden methods use File | Settings | File Templates.
+    }
+
+    public SpoutItem(ItemStack itemStack, String name, String texture, int textureSize, OddItemBase oddItemBase) {
+        this(oddItemBase, name, texture, textureSize);
+        this.itemStack = itemStack;
+        this.name = name;
+        this.texture = new Texture(oddItemBase, texture, textureSize, textureSize, 16);
+        this.oddItemBase = oddItemBase;
+        customBlocks.put(itemStack, this);
+    }
+
+    public static Map<ItemStack, GenericCubeCustomBlock> getCustomBlocks() {
+        return Collections.unmodifiableMap(customBlocks);
+    }
+}
